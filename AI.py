89ea79dd4e6975232fabb34ff3f65bd9e9de329f
Jamal <jamal.g16@hotmail.com>
2019-11-11 16:08:02 -0500

    Functional AI gameplay that only places, tries to block you and only checks current state of the game without seeing in the future

--- AI.py
+++ AI.py
@@ -1,10 +1,13 @@
 from Board import Board
+from Player import Player
+from TileColor import TileColor
 
-class AI:
 
-    def __init__(self, color):
-        self.color = color
-        self.placed_tokens = []
+class AI(Player):
+
+    def __init__(self, board, name, color, opponent_color):
+        super().__init__(board, name, color)
+        self.opponent_color = opponent_color
         # heuristic NODE with both a numerical value, and a decision type (move, place or none)
         self.current_heuristic_value = None
         self.inverse_letter_map = None
@@ -60,3 +63,126 @@ class AI:
         normal_letter_map = board.get_letter_map()
         self.inverse_letter_map = {v: k for k, v in normal_letter_map.items()}
 
+    def play_turn(self, moves):
+        heuristic_value = 0
+        position_to_play = None
+        for number in range(1, len(self.board.tiles)+1):
+            for letter in self.board.letterMap:
+                if self.board.get_tile(letter, number).get_color() == TileColor.BLANK:
+                    bottom_left_heuristic = self.get_bottom_left_heuristic(letter, number)
+                    top_left_heuristic = self.get_top_left_heuristic(letter, number)
+                    bottom_right_heuristic = self.get_bottom_right_heuristic(letter, number)
+                    top_right_heuristic = self.get_top_right_heuristic(letter, number)
+
+                    current_heuristic = 0
+                    if bottom_left_heuristic > 0:
+                        current_heuristic += bottom_left_heuristic
+                    if top_left_heuristic > 0:
+                        current_heuristic += top_left_heuristic
+                    if bottom_right_heuristic > 0:
+                        current_heuristic += bottom_right_heuristic
+                    if top_right_heuristic > 0:
+                        current_heuristic += top_right_heuristic
+
+                    if current_heuristic > heuristic_value:
+                        position_to_play = letter + str(number)
+
+        if position_to_play is not None:
+            self.play_tile(self.select_tile(position_to_play))
+        return 0
+
+    def get_bottom_left_heuristic(self, letter, number):
+        heuristic_value = 0
+        if self.board.is_valid_position((chr(ord(letter) - 1)), number - 1):  # try the bottom left
+            if self.board.get_tile((chr(ord(letter) - 1)),
+                                   number - 1).get_color() == self.opponent_color:
+                heuristic_value += 1
+                if self.board.is_valid_position((chr(ord(letter) - 2)), number - 2):  # try bottom left of bottom left
+                    if self.board.get_tile((chr(ord(letter) - 2)),
+                                           number - 2).get_color() == self.opponent_color:
+                        heuristic_value += 1
+
+                if self.board.is_valid_position((chr(ord(letter) - 2)), number):  # try top left of the bottom left
+                    if self.board.get_tile((chr(ord(letter) - 2)),
+                                           number).get_color() == self.opponent_color:
+                        heuristic_value += 1
+
+                if self.board.is_valid_position(letter, number - 2):  # try bottom right of the bottom left
+                    if self.board.get_tile(letter,
+                                           number - 2).get_color() == self.opponent_color:
+                        heuristic_value += 1
+            return heuristic_value
+        else:
+            return -1
+
+    def get_top_left_heuristic(self, letter, number):
+        heuristic_value = 0
+        if self.board.is_valid_position((chr(ord(letter) - 1)), number + 1):  # try the top left
+            if self.board.get_tile((chr(ord(letter) - 1)),
+                                   number + 1).get_color() == self.opponent_color:
+                heuristic_value += 1
+                if self.board.is_valid_position((chr(ord(letter) - 2)), number):  # try bottom left of the top left
+                    if self.board.get_tile((chr(ord(letter) - 2)),
+                                           number).get_color() == self.opponent_color:
+                        heuristic_value += 1
+
+                if self.board.is_valid_position((chr(ord(letter) - 2)), number + 2):  # try top left of the top left
+                    if self.board.get_tile((chr(ord(letter) - 2)),
+                                           number + 2).get_color() == self.opponent_color:
+                        heuristic_value += 1
+
+                if self.board.is_valid_position(letter, number + 2): # try top right of the bottom left
+                    if self.board.get_tile(letter,
+                                           number + 2).get_color() == self.opponent_color:
+                        heuristic_value += 1
+            return heuristic_value
+        else:
+            return -1
+
+    def get_bottom_right_heuristic(self, letter, number):
+        heuristic_value = 0
+        if self.board.is_valid_position((chr(ord(letter) + 1)), number - 1):  # try the bottom right
+            if self.board.get_tile((chr(ord(letter) + 1)),
+                                   number - 1).get_color() == self.opponent_color:
+                heuristic_value += 1
+                if self.board.is_valid_position(letter, number - 2):  # try bottom left of the bottom right
+                    if self.board.get_tile(letter,
+                                           number - 2).get_color() == self.opponent_color:
+                        heuristic_value += 1
+
+                if self.board.is_valid_position((chr(ord(letter) + 2)), number):  # try top right of the bottom right
+                    if self.board.get_tile((chr(ord(letter) + 2)),
+                                           number).get_color() == self.opponent_color:
+                        heuristic_value += 1
+
+                if self.board.is_valid_position((chr(ord(letter) + 2)), number - 2):  # try bottom right of bottom right
+                    if self.board.get_tile((chr(ord(letter) + 2)),
+                                           number - 2).get_color() == self.opponent_color:
+                        heuristic_value += 1
+            return heuristic_value
+        else:
+            return -1
+
+    def get_top_right_heuristic(self, letter, number):
+        heuristic_value = 0
+        if self.board.is_valid_position((chr(ord(letter) + 1)), number + 1):  # try the top right
+            if self.board.get_tile((chr(ord(letter) + 1)),
+                                   number + 1).get_color() == self.opponent_color:
+                heuristic_value += 1
+                if self.board.is_valid_position(letter, number + 2):  # try top left of the top right
+                    if self.board.get_tile(letter,
+                                           number + 2).get_color() == self.opponent_color:
+                        heuristic_value += 1
+
+                if self.board.is_valid_position((chr(ord(letter) + 2)), number + 2):  # try top right of the top right
+                    if self.board.get_tile((chr(ord(letter) + 2)),
+                                           number + 2).get_color() == self.opponent_color:
+                        heuristic_value += 1
+
+                if self.board.is_valid_position((chr(ord(letter) + 2)), number):  # try bottom right of the top right
+                    if self.board.get_tile((chr(ord(letter) + 2)),
+                                           number).get_color() == self.opponent_color:
+                        heuristic_value += 1
+            return heuristic_value
+        else:
+            return -1
